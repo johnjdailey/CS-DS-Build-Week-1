287079976e41e23722928e8fd43501d4e7129ea4
john <johnjdailey@gmail.com>
2020-10-15 15:07:16 -0400

    complete nbc

---
+++ README.md
@@ -0,0 +1,38 @@
+# CS Build Week 1 for Data Science Students
+
+# Naive Bayes Classifer Implementation
+
+Criteria: https://github.com/LambdaSchool/CS-Data-Science-Build-Week-1
+
+
+# Part I
+
+Naive Bayes Classifier - https://en.wikipedia.org/wiki/Naive_Bayes_classifier
+
+In this project the Naive Bayes Classifier algorithm is implemented as a Python class and has the required methods `fit` and `predict`. Only base Python, `numpy`, and `scipy` are allowed to be used for the implementation of the core of this Naive Bayes Classifier algorithm. 
+
+
+# Part II
+
+For the second part of this Build Week project, there is also a HOW-TO blog entry that describes the algorithm, how to implement it and what it's useful for here:
+
+blogpost-URL
+
+The reader should be able to begin implementation of the algorithm based on the information presented.
+
+
+# References:
+
+Naive Bayes Classifier - https://en.wikipedia.org/wiki/Naive_Bayes_classifier
+
+Naive Bayes Classifier From Scratch - https://machinelearningmastery.com/naive-bayes-classifier-scratch-python/
+
+How to Develop a Naive Bayes Classifier from Scratch in Python - https://machinelearningmastery.com/classification-as-conditional-probability-and-the-naive-bayes-algorithm/
+
+ML Primer - Why So Naive, Bayes? - https://www.confetti.ai/assets/ml-primer/ml_primer.pdf
+
+Wine UCI Dataset - https://archive.ics.uci.edu/ml/datasets/wine
+
+Scikit-learn Naive Bayes Source Code - https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/naive_bayes.py
+
+Using Probabilistic Machine Learning to improve your Stock Trading - https://medium.com/analytics-vidhya/using-probabilistic-machine-learning-to-improve-your-stock-trading-b40782f3710d
---
+++ naive_bayes_classifier.py
@@ -0,0 +1,150 @@
+#naive_bayes_classifier.py
+
+
+
+# Imports
+
+import numpy as np 
+
+
+# Naive Bayes Classifer Class
+
+class NaiveBayesClassifier:
+
+    def __init__(self):
+        pass
+
+    # Separate the dataset into a subset of data for each class
+
+    def separate_classes(self, X, y):
+        """
+        Separates the dataset in to a subset of data for each class.
+        Parameters:
+        ------------
+        X- array, list of features
+        y- list, target
+        Returns:
+        A dictionnary with y as keys and assigned X as values.
+        """
+        separated_classes = {}
+        for i in range(len(X)):
+            feature_values = X[i]
+            class_name = y[i]
+            if class_name not in separated_classes:
+                separated_classes[class_name] = []
+            separated_classes[class_name].append(feature_values)
+        return separated_classes
+
+    # Standard deviation and mean are required for the (Gaussian) distritbution function
+
+    def stat_info(self, X):
+        """
+        Calculates standard deviation and mean of features.
+        Parameters:
+        ------------
+        X- array , list of features
+        Returns:
+        A dictionary with STD and Mean as keys and assigned features STD and Mean as values.
+        """
+        for feature in zip(*X):
+            yield {
+                'std' : np.std(feature),
+                'mean' : np.mean(feature)
+            }
+    # Required fit method, to train the model
+
+    def fit (self, X, y):
+        """
+        Trains the model.
+        Parameters:
+        ----------
+        X: array-like, training features
+        y: list, target variable
+        Returns:
+        Dictionary with the prior probability, mean, and standard deviation of each class
+        """
+
+        separated_classes = self.separate_classes(X, y)
+        self.class_summary = {}
+
+        for class_name, feature_values in separated_classes.items():
+            self.class_summary[class_name] = {
+                'prior_proba': len(feature_values)/len(X),
+                'summary': [i for i in self.stat_info(feature_values)],
+            }
+        return self.class_summary
+
+    # Gaussian distribution function
+
+    def distribution(self, x, mean, std):
+        """
+        Gaussian Distribution Function
+        Parameters:
+        ----------
+        x: float, value of feature
+        mean: float, the average value of feature
+        stdev: float, the standard deviation of feature
+        Returns:
+        A value of Normal Probability
+        """
+
+        exponent = np.exp(-((x-mean)**2 / (2*std**2)))
+
+        return exponent / (np.sqrt(2*np.pi)*std)
+
+    # Required predict method, to predict the class
+
+    def predict(self, X):
+        """
+        Predicts the class.
+        Parameters:
+        ----------
+        X: array-like, test data set
+        Returns:
+        -----------
+        List of predicted class for each row of data set
+        """
+        MAPs = []
+
+        for row in X:
+            joint_proba = {}
+            
+            for class_name, features in self.class_summary.items():
+                total_features =  len(features['summary'])
+                likelihood = 1
+
+                for idx in range(total_features):
+                    feature = row[idx]
+                    mean = features['summary'][idx]['mean']
+                    stdev = features['summary'][idx]['std']
+                    normal_proba = self.distribution(feature, mean, stdev)
+                    likelihood *= normal_proba
+                prior_proba = features['prior_proba']
+                joint_proba[class_name] = prior_proba * likelihood
+
+            MAP = max(joint_proba, key= joint_proba.get)
+            MAPs.append(MAP)
+
+        return MAPs
+    
+    # Calculate the model's accuracy
+
+    def accuracy(self, y_test, y_pred):
+        """
+        Calculates model's accuracy.
+        Parameters:
+        ------------
+        y_test: actual values
+        y_pred: predicted values
+        Returns:
+        ------------
+        A number between 0-1, representing the percentage of correct predictions.
+        """
+
+        true_true = 0
+
+        for y_t, y_p in zip(y_test, y_pred):
+            if y_t == y_p:
+                true_true += 1 
+        return true_true / len(y_test)
+        
